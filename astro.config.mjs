73b7493212972e2f3d754df8076d80d541b9e67b
abdulkahhardogan <abdulkahhardogan@gmail.com>
2026-05-02 15:55:47 +0300

    Update GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://abdulkahhardogan.github.io',
-  base: '/AsminTech.Website',
+  base: '/AsminTech.Websitesi',
   i18n: {
     defaultLocale: 'tr',
     locales: ['tr', 'en'],
